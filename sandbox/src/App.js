81b8a3b2b7eea870769d6b94342b190bdfbf2329
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-12-08 12:43:13 +0300

    useRef to get previous to state

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import UseRef1 from './Components/UseRef1'
+import UseRef2 from './Components/UseRef2'
 
 function App() {
   return (
     <div className='container mt-5'>
-      <UseRef1/>
+      <UseRef2/>
     </div>
   )
 }
