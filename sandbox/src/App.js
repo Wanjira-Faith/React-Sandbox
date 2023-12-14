601abfb62e4637e227a820b6cd32b4cb2e123591
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-12-14 16:36:45 +0300

    useRef instance

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import UseRef1 from './Components/UseRef1'
 import UseRef2 from './Components/UseRef2'
+import UseRef3 from './Components/UseRef3'
+
 
 function App() {
   return (
     <div className='container mt-5'>
-      <UseRef2/>
+      <UseRef3/>
     </div>
   )
 }
