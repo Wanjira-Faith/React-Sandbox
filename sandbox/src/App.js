ae23ef77b9da96b2d67960ebd229bd9d2a5bcbc3
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-12-20 13:32:53 +0300

    modify file

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,10 +1,12 @@
 import UseCallback from './Components/UseCallback'
+import UseMemo from './Components/UseMemo'
 
 
 function App() {
   return (
     <div className='container mt-5'>
       <UseCallback/>
+      <UseMemo/>
     </div>
   )
 }
