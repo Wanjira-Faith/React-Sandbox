494eae8a58fd41dfa7f5e6a9265987f91028b206
Wanjira-Faith <wanjirafaith22@gmail.com>
2023-12-24 21:42:17 +0300

    update file

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,6 +1,7 @@
 import UseCallback from './Components/UseCallback'
 import UseMemo from './Components/UseMemo'
 import UseRef1 from './Components/UseRef1'
+import UseRef2 from './Components/UseRef2'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <UseCallback/>
       <UseMemo/>
       <UseRef1/>
+      <UseRef2/>
     </div>
   )
 }
